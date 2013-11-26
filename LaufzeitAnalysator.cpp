7fbe8f411f4b6e6f28c24e2d193f5f59e7e9890e
ufdnl <josh1992@hotmail.de>
2013-11-26 17:09:58 +0100

    LaufzeitAnalysator

--- LaufzeitAnalysator.cpp
+++ LaufzeitAnalysator.cpp
@@ -1,1 +1,13 @@
 #include "LaufzeitAnalysator.h"
+
+// Laufzeitformel vorbereiten
+void LaufzeitAnalysator::laufzeit( grundlaufzeit, lastFaktor, last, tempFaktor, spannFaktor, proFaktor) {
+    tempFaktor = Faktoren::getTemperatur();
+    spannFaktor = Faktoren::getSpannung();
+    proFaktor = Faktoren::getProzess();
+    lastFaktor = Faktoren::getFaktoren();
+    
+    int zeit = ( grundlaufzeit + lastFaktor * last ) * tempFaktor * spannFaktor * proFaktor;
+    
+    
+}
--- LaufzeitAnalysator.h
+++ LaufzeitAnalysator.h
@@ -3,13 +3,16 @@
 
 #include <iostream>
 #include "ListenElement.h"
+#include "GraphErzeuger.h"
 #include "Faktoren.h"
+#include "Bibliothek.h"
 using namespace std;
 
 class LaufzeitAnalysator {
 private:
+    void laufzeit( double, double, double, double, double, double,);
     
 public:
-
+    
 };
 #endif
