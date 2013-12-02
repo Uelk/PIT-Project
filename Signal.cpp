ae033f39650dad98fa12188f3303ff1bc15333d2
ufdnl <josh1992@hotmail.de>
2013-12-02 16:25:36 +0100

    Signal kommentiert

--- Signal.cpp
+++ Signal.cpp
@@ -16,42 +16,52 @@ Signal::Signal() {
 Signal::~Signal() {
 }
 
+// Rückgabe Anzahl Ziele
 int Signal::getAnzahlZiele() {
 	return anzahlZiele;
 }
 
+// Rückgabe Signaltyp
 Signal::signalTypen Signal::getSignalTyp() {
     return signalTyp;
 }
 
+// Rückgabe Quelle
 string Signal::getQuelle() {
     return quelle;
 }
 
+// Rückgabe Quellentyp
 string Signal::getQuellenTyp() {
     return quellenTyp;
 }
 
+// Rückgabe Ziel
 string Signal::getZiel( int pos ) {
     return ziele [pos];
 }
 
+// Anzahl der Ziele bestimmen
 void Signal::setAnzahlZiele( int nZiele ) {
     anzahlZiele = nZiele;
 }
 
+// Signaltyp bestimmen
 void Signal::setSignalTyp( signalTypen sigTyp ) {
     signalTyp = sigTyp;
 }
 
+// Quelle bestimmen
 void Signal::setQuelle( string gatterName ) {
     quelle = gatterName;
 }
 
+// Quellentyp bestimmen
 void Signal::setQuellenTyp( string gatterTyp ) {
     quellenTyp = gatterTyp;
 }
 
+// Ziel hinzufügen
 void Signal::zielHinzufuegen( string gatterName, int pos ) {
     ziele [pos] = gatterName;
 }
